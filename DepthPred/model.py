7cca3cf25c1d03258ec82a1da91431e0689de64a
SpencerLi <spencerli@autodrive.utoronto.ca>
2023-07-31 14:17:01 -0400

    Updated training scripts for depth model

--- DepthPred/model.py
+++ DepthPred/model.py
@@ -9,11 +9,6 @@ class coarseNet(nn.Module):
                  kernel_sizes=(11, 5, 3, 3, 3), strides=(4,1,1,1,2),
                  paddings=(0,2,1,1,0), name="coarseNet"):
         super(coarseNet, self).__init__()
-        # self.conv1 = nn.Conv2d(3, 96, kernel_size = 11, stride = 4, padding = 0)
-        # self.conv2 = nn.Conv2d(96, 256, kernel_size = 5, padding = 2)
-        # self.conv3 = nn.Conv2d(256, 384, kernel_size = 3, padding = 1)
-        # self.conv4 = nn.Conv2d(384, 384, kernel_size = 3, padding = 1)
-        # self.conv5 = nn.Conv2d(384, 256, kernel_size = 3, stride = 2)
         self.name = name
         self.stages = nn.ModuleList()
         self.stages.append(nn.Sequential(
@@ -31,45 +26,11 @@ class coarseNet(nn.Module):
                 nn.Conv2d(channels[i], channels[i+1], kernel_sizes[i+1], strides[i+1], paddings[i+1]),
                 nn.ReLU(inplace=True)
             ))
-        
-        # self.x, self.y = self.compute_fc_input(input_x=240, input_y=320, channels=channels,
-        #                                        kernel_sizes=kernel_sizes, strides=strides,
-        #                                        paddings=paddings)
-        # fc_input = self.x * self.y * channels[-1]
         # (6*9*256 = 13824)
         self.fc1 = nn.Linear(13824, 6144)
         self.fc2 = nn.Linear(6144, 4800)
         # 4800 = 60*80
         self.pool = nn.MaxPool2d(2)
-        self.dropout = nn.Dropout2d()
-
-    def compute_fc_input(input_x, input_y, channels=(96, 256, 384, 384, 256), 
-                         kernel_sizes=(11, 5, 3, 3, 3), strides=(4,1,1,1,2),
-                         paddings=(0,2,1,1,0)):
-        input_x = math.floor((input_x-kernel_sizes[0]+2*paddings[0])/strides[0])
-        input_x += 1
-        input_x  = math.floor(input_x/2)
-        input_x = math.floor((input_x-kernel_sizes[1]+2*paddings[1])/strides[1])
-        input_x += 1
-        input_x  = math.floor(input_x/2)
-        for i in range(2, len(kernel_sizes)):
-            input_x = math.floor((input_x-kernel_sizes[i]+2*paddings[i])/strides[i])
-            input_x += 1
-
-        input_y = math.floor((input_y-kernel_sizes[0]+2*paddings[0])/strides[0])
-        input_y += 1
-        input_y  = math.floor(input_y/2)
-        input_y = math.floor((input_y-kernel_sizes[1]+2*paddings[1])/strides[1])
-        input_y += 1
-        input_y  = math.floor(input_y/2)
-        for i in range(2, len(kernel_sizes)):
-            input_y = math.floor((input_y-kernel_sizes[i]+2*paddings[i])/strides[i])
-            input_y += 1
-
-        output_x, output_y = input_x, input_y
-        return output_x, output_y
-        
-
 
     def forward(self, x):
         """
@@ -80,36 +41,14 @@ class coarseNet(nn.Module):
             x = stage(x)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
         x = self.fc2(x)
         x = x.view(-1, 1, 60, 80)
         return x
-        #                                         # [n, c,  H,   W ]
-        #                                         # [8, 3, 228, 304]
-        # x = self.conv1(x)                       # [8, 96, 55, 74]
-        # x = F.relu(x)
-        # x = self.pool(x)                        # [8, 96, 27, 37] -- 
-        # x = self.conv2(x)                       # [8, 256, 23, 33]
-        # x = F.relu(x)
-        # x = self.pool(x)                        # [8, 256, 11, 16] 18X13
-        # x = self.conv3(x)                       # [8, 384, 9, 14]
-        # x = F.relu(x)
-        # x = self.conv4(x)                       # [8, 384, 7, 12]
-        # x = F.relu(x)
-        # x = self.conv5(x)                       # [8, 256, 5, 10] 8X5
-        # x = F.relu(x)
-        # x = x.view(x.size(0), -1)               # [8, 12800]
-        # x = F.relu(self.fc1(x))                 # [8, 4096]
-        # x = self.dropout(x)
-        # x = self.fc2(x)                         # [8, 4070]     => 55x74 = 4070
-        # x = x.view(-1, 1, 55, 74)
-        # return x
-                
-                
+
 class fineNet(nn.Module):
     def __init__(self, name="fineNet"):
         super(fineNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 63, kernel_size = 9, stride = 2, padding=3)
+        self.conv1 = nn.Conv2d(3, 63, kernel_size = 7, stride = 2, padding=3)
         self.conv2 = nn.Conv2d(64, 64, kernel_size = 5, padding = 2)
         self.conv3 = nn.Conv2d(64, 1, kernel_size = 5, padding = 2)
         self.pool = nn.MaxPool2d(2)
@@ -122,7 +61,7 @@ class fineNet(nn.Module):
         """
         x = F.relu(self.conv1(x))
         x = self.pool(x)
-        x = F.relu(self.conv2(x))
         x = torch.cat((x,y),1)
+        x = F.relu(self.conv2(x))
         x = self.conv3(x)
         return x
